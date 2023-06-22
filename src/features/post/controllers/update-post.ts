6110799f26e302c9c4b969bf15b2c0f13bcd4acf
Donatelo <lazar.kop@hotmail.com>
2023-06-22 21:15:24 +0200

    implemented images upload feature

--- src/features/post/controllers/update-post.ts
+++ src/features/post/controllers/update-post.ts
@@ -9,7 +9,7 @@ import { IPostDocument } from '@post/interfaces/post.interface';
 import { UploadApiResponse } from 'cloudinary';
 import { uploads } from '@global/helpers/cloudinary-upload';
 import { BadRequestError } from '@global/helpers/error-handler';
-//import { imageQueue } from '@service/queues/image.queue';
+import { imageQueue } from '@service/queues/image.queue';
 
 const postCache: PostCache = new PostCache();
 
@@ -89,13 +89,13 @@ export class Update {
     const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
     socketIOPostObject.emit('update post', postUpdated, 'posts');
     postQueue.addPostJob('updatePostInDB', { key: postId, value: postUpdated });
-    /* if (image) {
+    if (image) {
       imageQueue.addImageJob('addImageToDB', {
         key: `${req.currentUser!.userId}`,
         imgId: result.public_id,
         imgVersion: result.version.toString()
       });
-    } */
+    }
     return result;
   }
 }
