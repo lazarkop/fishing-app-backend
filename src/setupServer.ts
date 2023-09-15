6861cb8ca687a10b1d1f22b5bb8e6f023dc3f295
Donatelo <lazar.kop@hotmail.com>
2023-09-15 22:58:47 +0200

    ready for deployment

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -46,7 +46,8 @@ export class AppServer {
         name: 'session',
         keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: config.NODE_ENV !== 'development'
+        secure: config.NODE_ENV !== 'development',
+        sameSite: 'none'
       })
     );
     app.use(hpp());
